7152cf1373fce0da843ae217d94073043084fea5
Sujay <sujaybodhe99@gmail.com>
2022-05-27 01:36:14 +0530

    Updated styles and other things

--- app.js
+++ app.js
@@ -24,8 +24,9 @@ if(process.env.NODE_ENV !== 'production'){
    const userRoutes = require('./routes/users');
    const campgroundRoutes = require('./routes/campgrounds');
    const reviewRoutes = require('./routes/reviews');
-   //const dbUrl = process.env.DB_URL;
    
+   //const dbUrl = process.env.DB_URL;
+   //'mongodb://localhost:27017/yelp-camp'
    mongoose.connect('mongodb://localhost:27017/yelp-camp', {
        useNewUrlParser : true,
        useUnifiedTopology : true,
